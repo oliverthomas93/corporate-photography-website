cfa1cda2ecec6d1926257834666874e7b6eeb9b1
Oliver Thomas <63861649+oliverthomas93@users.noreply.github.com>
2020-08-18 17:26:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-var slideIndex = 1;
+//Slideshow
+let slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
+  let i;
+  let slides = document.getElementsByClassName("slides");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -19,3 +17,23 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+//Responsive side nav
+function navSlide() {
+    let burger = document.querySelector(".burger");
+    let nav = document.querySelector(".nav");
+    let navLinks = document.querySelectorAll(".page-links li, .sm-links li");
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        })
+    });
+}
+
+navSlide();
